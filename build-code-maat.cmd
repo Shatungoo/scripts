389da2e7138a29c5a221491d600b3fb537117b22
Leanid_miron@epam.com <Leanid_miron@epam.com>
2020-02-20 22:43:29 +0300

    add tag to build maat

--- build-code-maat.cmd
+++ build-code-maat.cmd
@@ -1,5 +1,5 @@
 git clone https://github.com/adamtornhill/code-maat.git
 cd .\code-maat\
 docker build -t maat . 
-docker create --name maat 
+docker create -t maat --name maat 
 docker cp maat:/usr/src/code-maat/app-standalone.jar ./
