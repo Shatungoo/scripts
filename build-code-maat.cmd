a06bef106ed28c8c5afd7c217cb00042eb778b23
LMiron <Leanid.miron@ibagroup.com>
2020-02-20 22:19:55 +0300

    add git clone to code maat

--- build-code-maat.cmd
+++ build-code-maat.cmd
@@ -1,4 +1,5 @@
-https://github.com/adamtornhill/code-maat.git
+git clone https://github.com/adamtornhill/code-maat.git
+cd .\code-maat\
 docker build -t maat . 
 docker create --name maat 
 docker cp maat:/usr/src/code-maat/app-standalone.jar ./
